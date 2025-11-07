c89696c321074b36334d8d9aaaeb27f4345141c8
your-username <aryansingh7910@gmail.com>
2025-11-07 12:47:36 +0530

    cart fixed

--- backend/src/data/cart.js
+++ backend/src/data/cart.js
@@ -1,13 +1,3 @@
-// dummy user
-// export const CURRENT_USER_ID = 'u1';
-
-// export const cart = {
-//   [CURRENT_USER_ID]: {
-//     items: [], // [{id,qty,price }]
-//     updatedAt: null
-//   }
-// };
-
 
 // Keep the cart in memory
 export const CART = [];
